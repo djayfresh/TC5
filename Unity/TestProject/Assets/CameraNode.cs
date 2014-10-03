ee0ceab3df1c969d4732e2b63d7dddb3ab0ec482
djayfresh <djfresh1@comcast.net>
2014-10-03 11:51:34 -0600

    Level, camera, pathing
    Make more micro commits

---
+++ Unity/TestProject/Assets/CameraNode.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraNode : MonoBehaviour {
+
+	public Camera camera;
+	public float waitTime;
+	public bool shouldmove;
+	public float cameraTimeHere;
+	// Use this for initialization
+	void Start () {
+		cameraTimeHere = 0;
+		camera = GetComponentInChildren<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool move()
+	{
+		cameraTimeHere += Time.deltaTime;
+		if (cameraTimeHere >= waitTime && shouldmove) 
+		{
+			cameraTimeHere = 0;
+						return true;
+				} else {
+						return false;
+				}
+	}
+}
